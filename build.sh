de4d90cabb008168f95294cf80eb80269fd081f9
MoeClub <32381827+MoeClub@users.noreply.github.com>
2021-09-07 14:18:30 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 apt update
 apt install -y make gcc linux-headers-$(uname -r)
 
-wget -qO /tmp/tcp_bbr.c "https://github.com/MoeClub/BBR/raw/master/src/$(uname -r |uname -r |cut -d"-" -f1)/tcp_bbr.c"
+wget -qO /tmp/tcp_bbr.c "https://github.com/MoeClub/BBR/raw/master/src/$(uname -r |cut -d"-" -f1)/tcp_bbr.c"
 wget -qO /tmp/Makefile "https://github.com/MoeClub/BBR/raw/master/Makefile"
 
 cd /tmp
