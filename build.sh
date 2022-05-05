fdc91db5f932af4e8c30ba5e8a92cf678e36fdea
MoeClub <32381827+MoeClub@users.noreply.github.com>
2022-05-05 11:03:44 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,7 +22,8 @@ kernelVer=$(uname -r |cut -d- -f1 |cut -d. -f1-2)
 wget -qO /tmp/tcp_bbr.c "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/net/ipv4/tcp_bbr.c?h=v${kernelVer}"
 [ $? -ne 0 ] && echo "Invalid Kernel Version." && exit 1
 
-wget -qO /tmp/Makefile "https://github.com/MoeClub/BBR/raw/master/Makefile"
+# wget -qO /tmp/Makefile "https://github.com/MoeClub/BBR/raw/master/Makefile"
+echo "b2JqLW0gOj0gdGNwX2Jici5vCgphbGw6CgltYWtlIC1DIC9saWIvbW9kdWxlcy9gdW5hbWUgLXJgL2J1aWxkIE09YHB3ZGAgbW9kdWxlcyBDQz1gd2hpY2ggZ2NjYAoJCmNsZWFuOgoJbWFrZSAtQyAvbGliL21vZHVsZXMvYHVuYW1lIC1yYC9idWlsZCBNPWBwd2RgIGNsZWFuCgppbnN0YWxsOgoJY3AgLXJmIHRjcF9iYnIua28gL2xpYi9tb2R1bGVzL2B1bmFtZSAtcmAva2VybmVsL25ldC9pcHY0CglpbnNtb2QgL2xpYi9tb2R1bGVzL2B1bmFtZSAtcmAva2VybmVsL25ldC9pcHY0L3RjcF9iYnIua28gMj4vZGV2L251bGwgfHwgdHJ1ZQoJZGVwbW9kIC1hCglzZWQgLWkgJy9uZXRcLmNvcmVcLmRlZmF1bHRfcWRpc2MvZCcgL2V0Yy9zeXNjdGwuY29uZgoJc2VkIC1pICcvbmV0XC5pcHY0XC50Y3BfY29uZ2VzdGlvbl9jb250cm9sL2QnIC9ldGMvc3lzY3RsLmNvbmYKCXdoaWxlIFsgLXogIiQkKHNlZCAtbiAnJCRwJyAvZXRjL3N5c2N0bC5jb25mKSIgXTsgZG8gc2VkIC1pICckJGQnIC9ldGMvc3lzY3RsLmNvbmY7IGRvbmUKCXNlZCAtaSAnJCRhXG5ldC5jb3JlLmRlZmF1bHRfcWRpc2MgPSBmcVxubmV0LmlwdjQudGNwX2Nvbmdlc3Rpb25fY29udHJvbCA9IGJiclxuXG4nIC9ldGMvc3lzY3RsLmNvbmYKCXN5c2N0bCAtcAoKdW5pbnN0YWxsOgoJcm0gLXJmIC9saWIvbW9kdWxlcy9gdW5hbWUgLXJgL2tlcm5lbC9uZXQvaXB2NC90Y3BfYmJyLmtvCglzZWQgLWkgJy9uZXRcLmNvcmVcLmRlZmF1bHRfcWRpc2MvZCcgL2V0Yy9zeXNjdGwuY29uZgoJc2VkIC1pICcvbmV0XC5pcHY0XC50Y3BfY29uZ2VzdGlvbl9jb250cm9sL2QnIC9ldGMvc3lzY3RsLmNvbmYKCXdoaWxlIFsgLXogIiQkKHNlZCAtbiAnJCRwJyAvZXRjL3N5c2N0bC5jb25mKSIgXTsgZG8gc2VkIC1pICckJGQnIC9ldGMvc3lzY3RsLmNvbmY7IGRvbmUKCXN5c2N0bCAtcAo=" |base64 -d >/tmp/Makefile
 [ $? -ne 0 ] && echo "Invalid Make File." && exit 1
 
 
