fab3c88fc3f3660e13d44c2cba88282eeb61659f
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-01-29 20:59:14 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,8 +19,12 @@ fi
 kernelVer=$(uname -r |cut -d- -f1 |cut -d. -f1-2)
 [ ! -n "${kernelVer}" ] && echo "No Found Kernel Version." && exit 1
 
-wget -qO /tmp/tcp_bbr.c "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/net/ipv4/tcp_bbr.c?h=v${kernelVer}"
-[ $? -ne 0 ] && echo "Invalid Kernel Version." && exit 1
+downloadOK=0
+[ "$downloadOK" == "0" ] && wget --no-check-certificate --timeout=10 -qO /tmp/tcp_bbr.c "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/net/ipv4/tcp_bbr.c?h=v${kernelVer}"
+[ $? -eq 0 ] && downloadOK=1
+[ "$downloadOK" == "0" ] && wget --no-check-certificate --timeout=10 -qO /tmp/tcp_bbr.c "https://raw.githubusercontent.com/torvalds/linux/refs/tags/v${kernelVer}/net/ipv4/tcp_bbr.c"
+[ $? -eq 0 ] && downloadOK=1
+[ "$downloadOK" -ne "1" ] && echo "Invalid Kernel Version." && exit 1
 
 # bbr_min_rtt_win_sec
 sed -i 's|static const u32 bbr_min_rtt_win_sec[^;]*;|static const u32 bbr_min_rtt_win_sec = 13;|g' /tmp/tcp_bbr.c
